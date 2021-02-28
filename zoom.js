f4dd32bc876d4fe425f13f756809605b04506ed9
Antonio <antoniorodriguez2002@gmail.com>
2021-02-28 22:32:35 +0100

    paginaweb

---
+++ zoom.js
@@ -0,0 +1,10 @@
+var options = {
+    width: 300,
+    height: 400,
+    zoomWidth: 450,
+    zoomPosition: "original",
+};
+
+var ImageZoom = new ImageZoom(document.getElementById("img-container"), options);
+
+ImageZoom.setup();
